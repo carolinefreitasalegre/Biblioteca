4d7603f3a5be601a29d2085dad4502efa5c72d7b
Carolinee <freitascaroline.92@gmail.com>
2025-12-12 06:41:25 -0400

    Remove bin/ and obj/ from repository

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5db7e6a6c8c570b6476ffa8f51dcf5421e892a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c12c6cfb312b151484db50c25476c05c78d75c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
