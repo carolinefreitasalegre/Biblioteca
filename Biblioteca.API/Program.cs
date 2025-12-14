657b9df57ad25adccf61c70dee64da56e98d0107
Carolinee <freitascaroline.92@gmail.com>
2025-12-14 08:17:46 -0400

    add json

--- Biblioteca.API/Program.cs
+++ Biblioteca.API/Program.cs
@@ -12,7 +12,10 @@ using Services.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile("appsettings.Development.json", optional: true)
+    .AddJsonFile("appsettings.Local.json", optional: true);
 
 //jwt
 builder.Services.AddAuthentication(opt =>
