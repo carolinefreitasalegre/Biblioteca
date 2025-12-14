354d53b8e05d17071653cc2ca48676ae49afa15c
Carolinee <freitascaroline.92@gmail.com>
2025-12-13 22:14:44 -0400

    subindo

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c12c6cfb312b151484db50c25476c05c78d75c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4471e8c5c8ad7e3f96d81277ffc088bcdaad9710")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
