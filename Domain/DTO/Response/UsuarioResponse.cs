9d259d2f85ef18739e5ce09b4f1d21151b000998
Carolinee <freitascaroline.92@gmail.com>
2025-12-18 16:03:30 -0400

    dialog (front) feito

--- Domain/DTO/Response/UsuarioResponse.cs
+++ Domain/DTO/Response/UsuarioResponse.cs
@@ -4,9 +4,11 @@ namespace Domain.DTO.Response;
 
 public class UsuarioResponse
 {
+    public int Id { get; set; }
     public string Nome { get; set; }
     public string Email { get; set; }
 
+    public EPerfilUsuario Role { get; set; }
     public EStatusUsuario Status { get; set; }
     public DateTime CriadoEm { get; set; }
 }
